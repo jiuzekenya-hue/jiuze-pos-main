4bf098df2eca8bd00b35de37edb924f6ad234ea5
jiuzekenya-hue <jiuzekenya@gmail.com>
2026-09-01 08:54:08 -0700

    feat: route sales history page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Categories from './pages/Categories'
 import Products from './pages/Products'
 import StockMovements from './pages/StockMovements'
 import Checkout from './pages/Checkout'
+import SalesHistory from './pages/SalesHistory'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path="/products" element={<ProtectedRoute><Products /></ProtectedRoute>} />
       <Route path="/stock-movements" element={<ProtectedRoute><StockMovements /></ProtectedRoute>} />
       <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
+      <Route path="/sales" element={<ProtectedRoute><SalesHistory /></ProtectedRoute>} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
