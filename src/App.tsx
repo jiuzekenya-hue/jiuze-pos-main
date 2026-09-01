beb777212092585f2cc55484135fcc09a1c610b5
jiuzekenya-hue <jiuzekenya@gmail.com>
2026-09-01 07:07:06 -0700

    feat(phase-4): add products route

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,15 @@ import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
 import AppShell from './pages/AppShell'
 import Categories from './pages/Categories'
+import Products from './pages/Products'
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <AppShell />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/categories"
-        element={
-          <ProtectedRoute>
-            <Categories />
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/" element={<ProtectedRoute><AppShell /></ProtectedRoute>} />
+      <Route path="/categories" element={<ProtectedRoute><Categories /></ProtectedRoute>} />
+      <Route path="/products" element={<ProtectedRoute><Products /></ProtectedRoute>} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
